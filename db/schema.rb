323cbe2ce076ae7ff61139fa05ec2c02fe05b5f3
Li Siyu <solafishes@gmail.com>
2012-12-07 01:06:41 +0800

    Add Micropost

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,11 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121206164515) do
+ActiveRecord::Schema.define(:version => 20121206165818) do
 
   create_table "microposts", :force => true do |t|
+    t.string   "content"
+    t.integer  "user_id"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
